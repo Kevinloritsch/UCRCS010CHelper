b6020eec5757110020d85fa33525c1e2f10306c3
anitathakim <anitathakim@gmail.com>
2025-01-30 16:50:03 -0800

    added bubble sort visualizer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import BSTVizualizer from "@/app/components/BSTVizualizer";
+import BubbleSortVisualizer from "./components/BubbleSortVisualizer";
 
 const Home = () => {
   return (
     <div>
       <BSTVizualizer />
+      <BubbleSortVisualizer />
     </div>
   );
 };
