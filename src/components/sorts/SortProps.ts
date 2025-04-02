e3cba31a2472217f60d547671780ef9633f64e38
anitathakim <anitathakim@gmail.com>
2025-04-02 12:38:32 -0700

    fixed react error

--- src/components/sorts/SortProps.ts
+++ src/components/sorts/SortProps.ts
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 import { bubbleSort } from "./bubble/BubbleSort";
 
 export const SortProps = (initialArr: number[] = []) => {
